81b65e02fce0b92dfa530521b21c37c8d399dd11
Adeola Ojo <ojoadeolagabriel@gmail.com>
2017-12-31 22:36:01 +0100

    cleaned up label on logs

--- src/main/java/com/interswitchgroup/web/HomeController.java
+++ src/main/java/com/interswitchgroup/web/HomeController.java
@@ -18,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -83,6 +84,11 @@ public class HomeController {
         CompletableFuture.allOf(tasks.toArray(new CompletableFuture[tasks.size()])).join();
         future.complete(modelAndView);
         return future;
+    }
+
+    @RequestMapping(value = "/error", method = RequestMethod.GET)
+    public String error() {
+        return "error";
     }
 
     public CompletableFuture<String> getStatus(String id) {
