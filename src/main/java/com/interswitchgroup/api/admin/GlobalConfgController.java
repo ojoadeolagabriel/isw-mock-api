212fd00e423601c0841389eabc68fd656bfdf582
Adeola Ojo <ojoadeolagabriel@gmail.com>
2018-01-02 02:00:48 +0100

    add ad-hoc config controller

---
+++ src/main/java/com/interswitchgroup/api/admin/GlobalConfgController.java
@@ -0,0 +1,18 @@
+package com.interswitchgroup.api.admin;
+
+import com.interswitchgroup.App;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("${route.admin.messages.url}")
+@RestController
+public class GlobalConfgController {
+    @RequestMapping("/config")
+    public ResponseEntity<String> get(){
+        HttpHeaders responseHeaders = new HttpHeaders();
+        return new ResponseEntity<>(App.adhocConfig.toString(), responseHeaders, HttpStatus.OK);
+    }
+}
