bdd329c4ce2112a30e5b8739a73f5ccbb71b3da9
adeola.ojo <ojoadeolagabriel@gmail.com>
2018-01-03 15:46:37 +0100

    updated admin url3

--- src/main/java/com/interswitchgroup/api/BaseController.java
+++ src/main/java/com/interswitchgroup/api/BaseController.java
@@ -2,10 +2,22 @@ package com.interswitchgroup.api;
 
 import org.springframework.http.HttpHeaders;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class BaseController {
     public HttpHeaders addContentType(HttpHeaders headers) {
         if (headers != null & !headers.containsKey("Content-type"))
             headers.add("Content-type", "application/json");
         return headers;
+    }
+    public String hostIp(){
+        String ip = "";
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return ip;
     }
 }
